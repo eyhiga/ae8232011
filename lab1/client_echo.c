a435728b3f33cfd5178c6bfeaf4b937ecc71d3da
alberto.a.oliv@gmail.com <alberto.a.oliv@gmail.com@b91f337c-4651-1274-c999-3ee97ab4b3d4>
2011-08-22 18:44:32 +0000

    Tinha esquecido de commitar. Alguns cleanups, for de teste no servidor, e arrumei as strings que estavam imprimindo lixo

--- lab1/client_echo.c
+++ lab1/client_echo.c
@@ -19,29 +19,34 @@
 
 int main(int argc, char *argv[])
 {
-    int sockfd;  
-	int numLinesSent=0;
-	int numLinesRcv=0;
-	int numBiggestLine=0;
-	int numCharsSent=0;
-	int numCharsRcv=0;
-	int charsSentAux = 0;
-	int charsRcvAux = 0;
+    int sockfd, i;
+    int numLinesSent=0;
+    int numLinesRcv=0;
+    int numBiggestLine=0;
+    int numCharsSent=0;
+    int numCharsRcv=0;
+    int charsSentAux = 0;
+    int charsRcvAux = 0;
     char buf[MAXDATASIZE];
-	char rcv[MAXDATASIZE];
+    char rcv[MAXDATASIZE];
     struct hostent *he;
     struct sockaddr_in their_addr; /* connector's address information */
 
+for(i=0;i<MAXDATASIZE;i++)buf[i] = '\0';
+for(i=0;i<MAXDATASIZE;i++)rcv[i] = '\0';
+printf("ok1!!\n");
     if (argc != 2) {
         fprintf(stderr,"usage: client hostname\n");
         exit(1);
     }
 
+printf("ok2!!\n");
     if ((he=gethostbyname(argv[1])) == NULL) {  /* get the host info */
         perror("gethostbyname");
         exit(1);
     }
 
+printf("ok3!!\n");
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(1);
@@ -50,44 +55,52 @@ int main(int argc, char *argv[])
     their_addr.sin_family = AF_INET;         /* host byte order */
     their_addr.sin_port = htons(PORT);     /* short, network byte order */
     their_addr.sin_addr = *((struct in_addr *)he->h_addr_list[0]);
+    printf("ok2!!\n");
     bzero(&(their_addr.sin_zero), 8);        /* zero the rest of the struct */
+
 
     if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1) {
         perror("connect");
         exit(1);
     }
 
+printf("ok4!!\n");
     /* First read to store data in cache. */
     while(fgets(buf, MAXDATASIZE, stdin) != NULL){
     }
     rewind(stdin);
 
-    while(fgets(buf, MAXDATASIZE, stdin) != NULL)
+    fgets(buf, MAXDATASIZE, stdin);
+    do
     {
-        charsSentAux = write(sockfd, buf, strlen(buf));
-		if(charsSentAux > 0) {
-			numLinesSent++;
-			numCharsSent += charsSentAux;
-			if(charsSentAux > numBiggestLine) {
-				numBiggestLine = charsSentAux;
-			}
-		}
+    charsSentAux = write(sockfd, buf, strlen(buf));
+    if(charsSentAux > 0) {
+        numLinesSent++;
+        numCharsSent += charsSentAux;
 
-
-        charsRcvAux = read(sockfd, rcv, MAXDATASIZE);
-		if(charsRcvAux > 0)
-		{
-			numLinesRcv++;
-			numCharsRcv += charsRcvAux;
-		}
-        //printf("%s", rcv);
+        if(charsSentAux > numBiggestLine) {
+            numBiggestLine = charsSentAux;
+        }
     }
 
-	fprintf(stderr, "Linhas enviadas: %d\n", numLinesSent);
-	fprintf(stderr, "Maior linha: %d\n", numBiggestLine);
-	fprintf(stderr, "Caracteres enviados: %d\n", numCharsSent);
-	fprintf(stderr, "Linhas recebidas: %d\n", numLinesRcv);
-	fprintf(stderr, "Caracteres recebidos: %d\n", numCharsRcv);
+    charsRcvAux = read(sockfd, rcv, MAXDATASIZE);
+        if(charsRcvAux > 0) {
+            rcv[charsRcvAux] = '\0';
+            printf("%s", rcv);
+            numLinesRcv++;
+            numCharsRcv += charsRcvAux;
+        }
+
+        buf[0]='\0';
+        fgets(buf, MAXDATASIZE, stdin);
+
+    }while(!feof(stdin));
+
+    fprintf(stderr, "Linhas enviadas: %d\n", numLinesSent);
+    fprintf(stderr, "Maior linha: %d\n", numBiggestLine);
+    fprintf(stderr, "Caracteres enviados: %d\n", numCharsSent);
+    fprintf(stderr, "Linhas recebidas: %d\n", numLinesRcv);
+    fprintf(stderr, "Caracteres recebidos: %d\n", numCharsRcv);
     close(sockfd);
 
     return 0;
