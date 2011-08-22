46fdfa74b10ca30a5aabc6ffa0f78af16275dc5b
eyhiga@gmail.com <eyhiga@gmail.com@b91f337c-4651-1274-c999-3ee97ab4b3d4>
2011-08-22 01:49:22 +0000

    Adding statistics

--- lab1/client_echo.c
+++ lab1/client_echo.c
@@ -19,8 +19,16 @@
 
 int main(int argc, char *argv[])
 {
-    int sockfd, numbytes;  
+    int sockfd;  
+	int numLinesSent=0;
+	int numLinesRcv=0;
+	int numBiggestLine=0;
+	int numCharsSent=0;
+	int numCharsRcv=0;
+	int charsSentAux = 0;
+	int charsRcvAux = 0;
     char buf[MAXDATASIZE];
+	char rcv[MAXDATASIZE];
     struct hostent *he;
     struct sockaddr_in their_addr; /* connector's address information */
 
@@ -41,7 +49,7 @@ int main(int argc, char *argv[])
 
     their_addr.sin_family = AF_INET;         /* host byte order */
     their_addr.sin_port = htons(PORT);     /* short, network byte order */
-    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr_list[0]);
     bzero(&(their_addr.sin_zero), 8);        /* zero the rest of the struct */
 
     if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1) {
@@ -56,11 +64,30 @@ int main(int argc, char *argv[])
 
     while(fgets(buf, MAXDATASIZE, stdin) != NULL)
     {
-        write(sockfd, buf, MAXDATASIZE);
-        read(sockfd, buf, MAXDATASIZE);
-        printf("%s", buf);
+        charsSentAux = write(sockfd, buf, MAXDATASIZE);
+		if(charsSentAux > 0) {
+			numLinesSent++;
+			numCharsSent += charsSentAux;
+			if(charsSentAux > numBiggestLine) {
+				numBiggestLine = charsSentAux;
+			}
+		}
+
+
+        charsRcvAux = read(sockfd, rcv, MAXDATASIZE);
+		if(charsRcvAux > 0)
+		{
+			numLinesRcv++;
+			numCharsRcv += charsRcvAux;
+		}
+        printf("%s", rcv);
     }
 
+	fprintf(stderr, "Linhas enviadas: %d\n", numLinesSent);
+	fprintf(stderr, "Maior linha: %d\n", numBiggestLine);
+	fprintf(stderr, "Caracteres enviados: %d\n", numCharsSent);
+	fprintf(stderr, "Linhas recebidas: %d\n", numLinesRcv);
+	fprintf(stderr, "Caracteres recebidos: %d\n", numCharsRcv);
     close(sockfd);
 
     return 0;
