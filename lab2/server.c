dfa8722ca974885a77e0e2637393ca4ab731b440
alberto.a.oliv <alberto.a.oliv@b91f337c-4651-1274-c999-3ee97ab4b3d4>
2011-09-13 01:21:55 +0000

    servidor

--- lab2/server.c
+++ lab2/server.c
@@ -24,12 +24,14 @@
 int main()
 {
     int i=0;
-    int sockfd, new_fd, numbytes;  /* sockfd espera por conexoes, new_fd serve para o envio de dados */
+    int sockfd, new_fd;  /* sockfd espera por conexoes, new_fd serve para o envio de dados */
     char line[MAXLINE];
     struct sockaddr_in my_addr;    /* informacao de endereco do servidor */
     struct sockaddr_in their_addr; /* informacoes do cliente  */
     unsigned int sin_size;
     int yes=1;
+    
+    FILE *rsock, *wsock;
 
     for(i=0;i<MAXLINE;i++)line[i] = '\0';
 
@@ -54,6 +56,7 @@ int main()
         perror("bind");
         exit(1);
     }
+    
     /* Escuta no socket apropriado*/
     if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
@@ -68,15 +71,25 @@ int main()
             perror("accept");
             continue;
         }
+        
+        if((rsock = fdopen(new_fd, "r")) == NULL){
+            perror("rsock");
+	    exit(1);
+        }
+        if((wsock = fdopen(new_fd, "w")) == NULL){
+            perror("wsock");
+	    exit(1);
+        }
+        
         fprintf(stderr, "server: got connection from %s\n",inet_ntoa(their_addr.sin_addr));
 	
 	/* Cria um processo filho para cuidar do envio na conexao estabelecida em new_fd*/
         if(!fork()){
-
-            while ((numbytes = read(new_fd, line, MAXLINE)) > 0) {
-                line[numbytes] = '\0';
+            while(fgets(line, MAXLINE, rsock) != NULL) {
+	        fflush(rsock);
                 printf("Linha Recebida: %s", line);
-                write(new_fd, line, numbytes);
+                fputs(line, wsock);
+                fflush(wsock);
             }
         close(new_fd);
 
