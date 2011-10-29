ee12825b7f0a189f2cd88ec814ac49e0a058b1c2
eyhiga@gmail.com <eyhiga@gmail.com@b91f337c-4651-1274-c999-3ee97ab4b3d4>
2011-10-29 14:18:07 +0000

    Adicionando arquivos do lab2

---
+++ trunk/lab4/client.c
@@ -0,0 +1,145 @@
+/*
+ ** client.c -- a stream socket client demo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <strings.h>
+#include <netdb.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/times.h>
+#include <netinet/in.h>
+
+#define PORT 9034    /* the port client will be connecting to */
+
+#define MAXDATASIZE 500 /* maximo de bytes que podem ser mandados de uma vez */
+
+int main(int argc, char *argv[])
+{
+
+    float telapsed;
+    clock_t start, end;
+    struct tms inicio, fim;
+    int sockfd, i;
+    int numLinesSent=0;
+    int numLinesRcv=0;
+    int numBiggestLine=0;
+    int numCharsSent=0;
+    int numCharsRcv=0;
+    int charsSentAux = 0;
+    int charsRcvAux = 0;
+    int success;
+    char *buf = malloc(MAXDATASIZE * sizeof(char));
+    char rcv[MAXDATASIZE];
+    char* rcvAux = NULL;
+    struct hostent *he; /* Extrai informacoes para conexao, como o nome, do servidor */
+    struct sockaddr_in their_addr; /* Guarda as informacoes do servidor conectando */
+
+    FILE *rsock, *wsock;
+
+    /* Limpa as strings de envio e recepcao */
+    for(i=0;i<MAXDATASIZE;i++)buf[i] = '\0';
+    for(i=0;i<MAXDATASIZE;i++)rcv[i] = '\0';
+
+    if (argc != 2) {
+        fprintf(stderr,"usage: client hostname\n");
+        exit(1);
+    }
+
+
+    if ((he=gethostbyname(argv[1])) == NULL) {  /* Pega informacoes do servidor e guarda em he */
+        perror("gethostbyname");
+        exit(1);
+    }
+
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { /* cria um novo socket de conexao */
+        perror("socket");
+        exit(1);
+    }
+    
+    /* abre ambos os descritores de arquivo no socket a ser usado para o envio e recebimento */
+    rsock = fdopen(sockfd, "r");
+    wsock = fdopen(sockfd, "w");
+
+    their_addr.sin_family = AF_INET;       /* Ordem dos bytes do host */
+    their_addr.sin_port = htons(PORT);     /* Ordem dos bytes da rede */
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr_list[0]);
+    bzero(&(their_addr.sin_zero), 8);
+
+    /* Conecta usando o socket criado e as informacoes extraidas do servidor */
+    if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1) {
+        perror("connect");
+        exit(1);
+    }
+
+    /* Primeira leitura para guardar arquivo na cache */
+    while(fgets(buf, MAXDATASIZE, stdin) != NULL){
+    }
+    rewind(stdin);
+
+    start = times(&inicio); /* Inicio da contagem de tempo */
+
+    if(!fork())
+    {
+        // Processo filho - Envio
+        while(fgets(buf, MAXDATASIZE, stdin) != NULL)
+        {
+            success = fputs(buf, wsock);
+            fflush(wsock);
+
+            if(success > 0)
+            {
+                charsSentAux = strlen(buf);
+                numCharsSent += charsSentAux;
+                numLinesSent++;
+
+                if(charsSentAux > numBiggestLine)
+                {
+                    numBiggestLine = charsSentAux;
+                }
+
+            }
+        }
+        fputs(buf, wsock);
+	
+	/* Fechamento do socket */
+        shutdown(sockfd, SHUT_WR);
+	
+	/* Estatisticas de envio do processo filho */
+        fprintf(stderr, "Linhas enviadas: %d\n", numLinesSent);
+    	fprintf(stderr, "Maior linha: %d\n", numBiggestLine);
+    	fprintf(stderr, "Caracteres enviados: %d\n", numCharsSent);
+        exit(0);
+    }
+
+    /* Processo pai - Laço de recebimento de dados */
+    rcvAux = fgets(rcv, MAXDATASIZE, rsock);
+    while(rcvAux != NULL)
+    {
+        charsRcvAux = strlen(rcvAux);
+        numCharsRcv += charsRcvAux;
+        numLinesRcv++;
+        printf("%s", rcv);
+        rcvAux = fgets(rcv, MAXDATASIZE, rsock);
+        fflush(rsock);
+    }
+
+    end = times(&fim);
+    telapsed = (float)(end-start) / sysconf(_SC_CLK_TCK); /* termina contagem de tempo */
+    
+    /* Estatisticas de recebimento do processo pai */
+    fprintf(stderr, "Tempo total: %4.1f s\n", telapsed);
+    fprintf(stderr, "Linhas recebidas: %d\n", numLinesRcv);
+    fprintf(stderr, "Caracteres recebidos: %d\n", numCharsRcv);
+    close(sockfd);
+    fclose(rsock);
+
+    return 0;
+
+}
---
+++ trunk/lab4/server.c
@@ -0,0 +1,116 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <strings.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+
+#define MYPORT 9034    /* porta usada para a conexao */
+
+#define BACKLOG 10     /* maximo de conexoes que podem ficar na fila  */
+
+#define MAXLINE 500   /* Tamanho da linha recebida */
+
+int main()
+{
+    int i=0;
+    int sockfd, new_fd;  /* sockfd espera por conexoes, new_fd serve para o envio de dados */
+    char line[MAXLINE];
+    struct sockaddr_in my_addr;    /* informacao de endereco do servidor */
+    struct sockaddr_in their_addr; /* informacoes do cliente  */
+    unsigned int sin_size;
+    int yes=1;
+    
+    /* Descritores de arquivo que serão abertos no socket */
+    FILE *rsock, *wsock;
+
+    for(i=0;i<MAXLINE;i++)line[i] = '\0';
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    my_addr.sin_family = AF_INET;         /* Ordem dos bytes do host */
+    my_addr.sin_port = htons(MYPORT);     /* Ordem dos bytes da rede */
+    my_addr.sin_addr.s_addr = INADDR_ANY; /* Preenche com IP do server */
+    bzero(&(my_addr.sin_zero), 8);
+
+    /* Seta informacoes do server para o socket */
+    if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) {
+        perror("bind");
+        exit(1);
+    }
+    
+    /* Escuta no socket apropriado*/
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    while(1) {  /* main accept() loop */
+        sin_size = sizeof(struct sockaddr_in);
+	
+	/* Aceita novas conexoes no socket de envio, atraves dos dados recebidos pelo socket de escuta */
+        if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1) {
+            perror("accept");
+            continue;
+        }
+        
+        /* Abre ambos os descritores de arquivo no socket usado para envio e recebimento */
+        if((rsock = fdopen(new_fd, "r")) == NULL){
+            perror("rsock");
+	    exit(1);
+        }
+        if((wsock = fdopen(new_fd, "w")) == NULL){
+            perror("wsock");
+	    exit(1);
+        }
+        
+        fprintf(stderr, "server: got connection from %s\n",inet_ntoa(their_addr.sin_addr));
+	
+	/* Cria um processo filho para cuidar do envio na conexao estabelecida em new_fd */
+        if(!fork()){
+	    
+	    /* Loop de recebimento e ecoamento */
+            while ((fgets(line, MAXLINE, rsock)) != NULL) {
+                line[strlen(line)] = '\0';
+                fflush(rsock);
+                printf("Linha Recebida: %s", line);
+                fputs(line, wsock);
+                fflush(wsock);
+            }
+            close(new_fd);
+
+            fprintf(stderr, "server: connection from %s closed\n",inet_ntoa(their_addr.sin_addr));
+            exit(0);
+        }
+        
+        /* Fecha o socket de envio e recebimento */
+        close(new_fd);
+
+        while(waitpid(-1, NULL, WNOHANG) > 0);
+
+
+    }
+    return 0;
+
+}
+
+
